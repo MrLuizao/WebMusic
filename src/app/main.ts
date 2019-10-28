cf103e25d18188083ff13885ccc5fb8c85a8daa0
Luis <Vazquez>
2019-10-28 12:58:12 -0600

    changes config environment deploy

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,8 +1,10 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { environment } from 'src/environments/environment.prod';
+import { AppModule } from './app.module';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+// import { AppModule } from './app/app.module';
+// import { environment } from './environments/';
 
 if (environment.production) {
   enableProdMode();
